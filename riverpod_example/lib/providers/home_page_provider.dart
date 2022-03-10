19eb84c05d9cc1ab8bba2b5b562b04c9b70708fb
Joep Heijnen <joep.heijnen@topicus.nl>
2022-03-10 15:23:04 +0100

    Save

---
+++ riverpod_example/lib/providers/home_page_provider.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// final counterProvider = StateNotifierProvider<CounterNotifier, int>((ref) {
+//   return CounterNotifier(ref);
+// });
+
+// class CounterNotifier extends StateNotifier<int> {
+//   final Ref ref;
+
+//   CounterNotifier(this.ref) : super(0);
+
+//   void increment() {
+//     state++;
+//   }
+
+//   void decrement() {
+//     state--;
+//   }
+// }
