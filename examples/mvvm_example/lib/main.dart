9c21cc4dbeac0a95b2ce800d3028e159256dedb0
Joep Heijnen <joep.heijnen@topicus.nl>
2022-03-18 14:58:16 +0100

    Moved services

--- examples/mvvm_example/lib/main.dart
+++ examples/mvvm_example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mvvm_example/models/services/crypto_service.dart';
+import 'package:mvvm_example/services/crypto_service.dart';
 import 'package:mvvm_example/views/coins_page.dart';
 
 void main() {
--- examples/mvvm_example/lib/models/coin/coin_repository.dart
+++ examples/mvvm_example/lib/models/coin/coin_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mvvm_example/models/coin/coin.dart';
-import 'package:mvvm_example/models/services/crypto_service.dart';
+import 'package:mvvm_example/services/crypto_service.dart';
 
 class CoinRepository {
   final CryptoService _cryptoService = GetIt.instance.get<CryptoService>();
