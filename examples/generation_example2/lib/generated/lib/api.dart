4b324936e3f8762ecbdbb1553c7e5611dd6e4d6f
Joep Heijnen <joep.heijnen@topicus.nl>
2022-03-24 15:14:41 +0100

    Update names

---
+++ examples/generation_example2/lib/generated/lib/api.dart
@@ -0,0 +1,30 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.0
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+library openapi.api;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:meta/meta.dart';
+
+part 'api_helper.dart';
+
+
+part 'model/account.dart';
+
+
+const _delimiters = {'csv': ',', 'ssv': ' ', 'tsv': '\t', 'pipes': '|'};
+const _dateEpochMarker = 'epoch';
+final _regList = RegExp(r'^List<(.*)>$');
+final _regSet = RegExp(r'^Set<(.*)>$');
+final _regMap = RegExp(r'^Map<String,(.*)>$');
+
---
+++ examples/generation_example2/rest_version_generation/build.sh
@@ -0,0 +1,20 @@
+java -jar openapi-generator-cli-5.4.0.jar generate -i parro.yaml -g dart -c open-generator-config.yaml --global-property models,supportingFiles --enable-post-process-file
+rm -r ../lib/generated/lib/auth
+rm -r ../lib/generated/lib/api_client.dart
+rm -r ../lib/generated/lib/api_exception.dart
+
+sed -i "/import 'package:http\/http\.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/import 'package:intl\/intl\.dart';/d" ../lib/generated/lib/api.dart
+
+sed -i "/part 'api_client.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'api_exception.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'auth\/authentication.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'auth\/api_key_auth.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'auth\/oauth.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'auth\/http_bearer_auth.dart';/d" ../lib/generated/lib/api.dart
+sed -i "/part 'auth\/http_basic_auth.dart';/d" ../lib/generated/lib/api.dart
+
+sed -i "/final _dateFormatter = DateFormat('yyyy-MM-dd');/d" ../lib/generated/lib/api.dart
+sed -i "/ApiClient defaultApiClient = ApiClient();/d" ../lib/generated/lib/api.dart
+
+sed -i '65,73d' ../lib/generated/lib/api_helper.dart
