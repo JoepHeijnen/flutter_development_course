f1261e69c7238f60e502db12a2be9171f9a9a9c2
Joep Heijnen <joep.heijnen@topicus.nl>
2022-03-25 13:37:17 +0100

    Update

--- ibmi/lib/pages/bmi_page.dart
+++ ibmi/lib/pages/bmi_page.dart
@@ -12,31 +12,44 @@ class BMIPage extends StatefulWidget {
 class _BMIPageState extends State<BMIPage> {
   int _age = 25;
   int _weight = 160;
+  int _height = 70;
+  int _gender = 0;
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
+      child: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              _ageSelectContainer(),
-              _weightSelectContainer(),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _ageSelectContainer(),
+                  const SizedBox(width: 16),
+                  _weightSelectContainer(),
+                ],
+              ),
+              const SizedBox(height: 16),
+              _heightSelectContainer(),
+              const SizedBox(height: 16),
+              _genderSelectContainer(),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
 
   Widget _ageSelectContainer() {
     return InfoCard(
+      flex: 1,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
@@ -94,6 +107,7 @@ class _BMIPageState extends State<BMIPage> {
 
   Widget _weightSelectContainer() {
     return InfoCard(
+      flex: 1,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
@@ -144,6 +158,70 @@ class _BMIPageState extends State<BMIPage> {
               ),
             ],
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _heightSelectContainer() {
+    return InfoCard(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Height in',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+          ),
+          Text(
+            _height.toString(),
+            style: const TextStyle(fontSize: 45, fontWeight: FontWeight.w700),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: CupertinoSlider(
+              min: 0,
+              max: 100,
+              divisions: 100,
+              value: _height.toDouble(),
+              onChanged: (_value) {
+                setState(() {
+                  _height = _value.toInt();
+                });
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _genderSelectContainer() {
+    return InfoCard(
+      flex: 1,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'gender',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+          ),
+          CupertinoSlidingSegmentedControl(
+            groupValue: _gender,
+            children: const {
+              0: Text('Male'),
+              1: Text('Female'),
+            },
+            onValueChanged: (_value) {
+              setState(() {
+                _gender = _value as int;
+              });
+            },
+          ),
         ],
       ),
     );
--- ibmi/lib/widgets/info_card.dart
+++ ibmi/lib/widgets/info_card.dart
@@ -1,27 +1,31 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InfoCard extends StatelessWidget {
   final Widget child;
+  final int flex;
 
-  const InfoCard({Key? key, required this.child}) : super(key: key);
+  const InfoCard({Key? key, required this.child, required this.flex})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black26,
-            blurRadius: 5,
-          ),
-        ],
+    return Flexible(
+      flex: flex,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 5,
+            ),
+          ],
+        ),
+        child: child,
       ),
-      child: child,
     );
   }
 }
