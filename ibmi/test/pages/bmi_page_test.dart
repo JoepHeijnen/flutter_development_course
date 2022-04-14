f2e43aaabd98fe3d975b66f3c17d8a08144cfa2b
Joep Heijnen <joep.heijnen@topicus.nl>
2022-04-14 11:36:04 +0200

    Added tests

--- ibmi/test/pages/bmi_page_test.dart
+++ ibmi/test/pages/bmi_page_test.dart
@@ -3,8 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ibmi/pages/bmi_page.dart';
 
 void main() {
-  testWidgets(
-      'Given weightInfoCard when user click + button then weight increment by 1',
+  testWidgets('Given weightInfoCard when user click + button then weight increment by 1',
       (tester) async {
     await tester.pumpWidget(
       const CupertinoApp(
@@ -21,8 +20,7 @@ void main() {
     expect(_text, findsOneWidget);
   });
 
-  testWidgets(
-      'Given weightInfoCard when user click - button then weight decrement by 1',
+  testWidgets('Given weightInfoCard when user click - button then weight decrement by 1',
       (tester) async {
     await tester.pumpWidget(
       const CupertinoApp(
@@ -30,12 +28,60 @@ void main() {
       ),
     );
 
-    Finder _weightIncrementButton = find.byKey(const Key('weight_minus'));
+    Finder _weightDecrementButton = find.byKey(const Key('weight_minus'));
 
-    await tester.tap(_weightIncrementButton);
+    await tester.tap(_weightDecrementButton);
     await tester.pump();
 
     Finder _text = find.text('159');
+    expect(_text, findsOneWidget);
+  });
+
+  testWidgets('Given ageInfoCard when user click + button then age increment by 1', (tester) async {
+    await tester.pumpWidget(
+      const CupertinoApp(
+        home: BMIPage(),
+      ),
+    );
+
+    Finder _ageIncrementButton = find.byKey(const Key('age_plus'));
+
+    await tester.tap(_ageIncrementButton);
+    await tester.pump();
+
+    Finder _text = find.text('26');
+    expect(_text, findsOneWidget);
+  });
+
+  testWidgets('Given ageInfoCard when user click - button then age decrement by 1', (tester) async {
+    await tester.pumpWidget(
+      const CupertinoApp(
+        home: BMIPage(),
+      ),
+    );
+
+    Finder _ageDecrementButton = find.byKey(const Key('age_minus'));
+
+    await tester.tap(_ageDecrementButton);
+    await tester.pump();
+
+    Finder _text = find.text('24');
+    expect(_text, findsOneWidget);
+  });
+
+  testWidgets('Given heightInfoCard when user slide then height increment by 1', (tester) async {
+    await tester.pumpWidget(
+      const CupertinoApp(
+        home: BMIPage(),
+      ),
+    );
+
+    Finder _heightSlider = find.byKey(const Key('height_slider'));
+
+    await tester.drag(_heightSlider, const Offset(2, 0));
+    await tester.pump();
+
+    Finder _text = find.text('72');
     expect(_text, findsOneWidget);
   });
 }
